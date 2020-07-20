586e46faa8f2a36c18294580451d0e45065ce554
Wilebaldo Santana Hernández <wisahe@gmail.com>
2020-07-20 15:50:00 -0600

    Cambiamos los tipos de datos de algunas columnas

--- csv_to_mysql.py
+++ csv_to_mysql.py
@@ -15,23 +15,49 @@ from sqlalchemy import create_engine
 # Debido a que los datos del archivo CSV no tiene encabezados, creamos una lista
 # con los nombres de todas las columnas.
 columnas = [
-    'id', 'codigo_entidad', 'entidad', 'nombre_escuela', 'cct', 'turno', 'municipio', 'localidad',
-    'tipo_escuela', 'grado_evaluado', 'grado_marginacion', 'alumnos_programados',
-    'lyc_alumnos_evaluados', 'mat_alumnos_evaluados',
-    'lyc_porcentaje_evaluados', 'mat_porcentaje_evaluados',
-    'lyc_representan_totalidad', 'mat_representan_totalidad',
-    'escuela_parecidas', 'lyc_logro_alumnos_i',
-    'lyc_logro_alumnos_ii', 'lyc_logro_alumnos_iii',
-    'lyc_logro_alumnos_iv', 'lyc_logro_porcentajes_i',
-    'lyc_logro_porcentajes_ii', 'lyc_logro_porcentajes_iii',
-    'lyc_logro_porcentajes_iv', 'lyc_logro_escparecidas_i',
-    'lyc_logro_escparecidas_ii', 'lyc_logro_escparecidas_iii',
-    'lyc_logro_escparecidas_iv', 'mat_logro_alumnos_i',
-    'mat_logro_alumnos_ii', 'mat_logro_alumnos_iii',
-    'mat_logro_alumnos_iv', 'mat_logro_porcentajes_i',
-    'mat_logro_porcentajes_ii', 'mat_logro_porcentajes_iii',
-    'mat_logro_porcentajes_iv', 'mat_logro_escparecidas_i',
-    'mat_logro_escparecidas_ii', 'mat_logro_escparecidas_iii', 'mat_logro_escparecidas_iv'
+    'id',
+    'codigo_entidad',
+    'entidad',
+    'nombre_escuela',
+    'cct',
+    'turno',
+    'municipio',
+    'localidad',
+    'tipo_escuela',
+    'grado_evaluado',
+    'grado_marginacion',
+    'alumnos_programados',
+    'lyc_alumnos_evaluados',
+    'mat_alumnos_evaluados',
+    'lyc_porcentaje_evaluados',
+    'mat_porcentaje_evaluados',
+    'lyc_representan_totalidad',
+    'mat_representan_totalidad',
+    'escuela_parecidas',
+    'lyc_logro_cantidad_alumnos_i',
+    'lyc_logro_cantidad_alumnos_ii',
+    'lyc_logro_cantidad_alumnos_iii',
+    'lyc_logro_cantidad_alumnos_iv',
+    'lyc_logro_porcentajes_i',
+    'lyc_logro_porcentajes_ii',
+    'lyc_logro_porcentajes_iii',
+    'lyc_logro_porcentajes_iv',
+    'lyc_logro_escuelas_parecidas_i',
+    'lyc_logro_escuelas_parecidas_ii',
+    'lyc_logro_escuelas_parecidas_iii',
+    'lyc_logro_escuelas_parecidas_iv',
+    'mat_logro_cantidad_alumnos_i',
+    'mat_logro_cantidad_alumnos_ii',
+    'mat_logro_cantidad_alumnos_iii',
+    'mat_logro_cantidad_alumnos_iv',
+    'mat_logro_porcentajes_i',
+    'mat_logro_porcentajes_ii',
+    'mat_logro_porcentajes_iii',
+    'mat_logro_porcentajes_iv',
+    'mat_logro_escuelas_parecidas_i',
+    'mat_logro_escuelas_parecidas_ii',
+    'mat_logro_escuelas_parecidas_iii',
+    'mat_logro_escuelas_parecidas_iv'
 ]
 
 # Leemos el archivo CSV (Coma Separate Values) y le asignamos
@@ -41,6 +67,11 @@ df = pd.read_csv('datos_csv/nac_escuelas_peb2019.csv', names=columnas)
 # Creamos la conexión a la base de datos en MySQL
 # En este caso, conectamos a la base de datos 'peb2019'
 engine = create_engine('mysql+pymysql://root:w64xstvlmr.@localhost/peb2019')
+
+# Modificamos los tipos de datos de algunas columnas, de int64 a float
+df = df.astype({'lyc_logro_escuelas_parecidas_i': float,
+                'lyc_logro_escuelas_parecidas_ii': float, 'lyc_logro_escuelas_parecidas_iii': float, 'lyc_logro_escuelas_parecidas_iv': float, 'mat_logro_escuelas_parecidas_i': float,
+                'mat_logro_escuelas_parecidas_ii': float, 'mat_logro_escuelas_parecidas_iii': float, 'mat_logro_escuelas_parecidas_iv': float, })
 
 # Escribimos el DataFrame a la tabla 'peb19' en la base de datos
 df.to_sql('peb19', engine, index=False)
